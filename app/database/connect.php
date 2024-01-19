6dd51d844d51b5d0c92f6dc8054ee46d30984c72
y045u <fukasawa0405.tech@gmail.com>
2024-01-20 05:17:20 +0900

    DBconnect成功

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,6 +1,15 @@
 <?php
 
+$user = "root";
+$pass = "rootpas";
+
 // DB接続
-$pdo = new PDO('mysql:host=localhost;dbname=test', $user, $pass);
+try {
+	$pdo = new PDO('mysql:host=localhost;dbname=2chan-clone', $user, $pass);
+	// echo "DB接続成功";
+} catch (PDOException $error) {
+	echo $error->getMessage();
+} 
+
 
 ?>
