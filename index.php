13e945f5da3a0db2e7b68a0d39b0f32126837537
y045u <fukasawa0405.tech@gmail.com>
2024-01-20 22:31:43 +0900

    エスケープ処理を追加

--- index.php
+++ index.php
@@ -16,11 +16,17 @@ if(isset($_POST["submitButton"])) {
 	// お名前入力チェック
 	if(empty($_POST["username"])) {
 		$error_message["username"] = "お名前を入力してください。";
+	} else {
+		// エスケープ処理
+		$escaped["username"] = htmlspecialchars($_POST["username"], ENT_QUOTES, "UTF-8");
 	}
 	
 	// コメント入力チェック
 	if(empty($_POST["body"])) {
 		$error_message["body"] = "コメントを入力してください。";
+	} else {
+		// エスケープ処理
+		$escaped["body"] = htmlspecialchars($_POST["body"], ENT_QUOTES, "UTF-8");
 	}
 
 	if(empty($error_message)) {
@@ -31,8 +37,8 @@ if(isset($_POST["submitButton"])) {
 		$statement = $pdo->prepare($sql);
 		
 		// 値をセットする
-		$statement->bindParam(":username", $_POST["username"], PDO::PARAM_STR);
-		$statement->bindParam(":body", $_POST["body"], PDO::PARAM_STR);
+		$statement->bindParam(":username", $escaped["username"], PDO::PARAM_STR);
+		$statement->bindParam(":body", $escaped["body"], PDO::PARAM_STR);
 		$statement->bindParam(":post_date", $post_date, PDO::PARAM_STR);
 		
 		$statement->execute();
